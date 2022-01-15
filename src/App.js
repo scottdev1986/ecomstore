cedf563b7466dad7524093421449163ae0afd9bb
Scott <88298032+scottdev1986@users.noreply.github.com>
2022-01-15 13:00:47 -0500

    adding Products and Ratings

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import HomeScreen from './screens/HomeScreen'
+
 function App() {
   return (
     <div>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to EcomStore</h1>
+          <HomeScreen> </HomeScreen>
         </Container>
       </main>
       <Footer />
