0f107a279b1495258f492d868a94935afbde8d91
Scott <88298032+scottdev1986@users.noreply.github.com>
2022-01-15 18:46:54 -0500

    starting django backend change file struct

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def get_routes(request):
-    
+    routes = [
+        
+    ]
     return JsonResponse('Hello', safe=False)
  
