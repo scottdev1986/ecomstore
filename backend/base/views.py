54936c9a8a7f45547097ada6a064507f146d01f7
Scott <88298032+scottdev1986@users.noreply.github.com>
2022-01-15 22:09:57 -0500

    adding serializer

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,12 @@
+from itertools import product
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def get_routes(request):
@@ -25,15 +28,14 @@ def get_routes(request):
 
 @api_view(['GET'])
 def get_products(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_product(request, pk):
-    product = None
+    product = Product.objects.get(_id=pk)
+    serializer =ProductSerializer(product, many=False)
 
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break   
-
-    return Response(product)
+    return Response(serializer.data)
