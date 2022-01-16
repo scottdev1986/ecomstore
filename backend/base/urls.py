9b00e4857bf191dc1ba25279d5664d77e2b2d3bd
Scott <88298032+scottdev1986@users.noreply.github.com>
2022-01-15 19:48:46 -0500

    adding in django rest api calls

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_routes, name='routes')
+    path('', views.get_routes, name='routes'),
+    path('products/', views.get_products, name='products'),
+    path('products/<str:pk>/', views.get_product, name='product'),
 ]
